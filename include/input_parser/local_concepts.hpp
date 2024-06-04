9d67817838b2b7ebffd601b79d33e8d53c7cd485
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2024-06-04 13:19:58 +0100

    feat, style: same_as_any and concept with snake_case

--- include/input_parser/local_concepts.hpp
+++ include/input_parser/local_concepts.hpp
@@ -7,12 +7,21 @@
 namespace input_parser {
 
 /**
+ * @brief A concept that checks if the type is the same as any of the provided
+ * types.
+ * @tparam Type The type to check.
+ * @tparam Types The types to compare with.
+ */
+template <typename Type, typename... Types>
+concept same_as_any = (... || std::is_same_v<Type, Types>);
+
+/**
  * @brief Checks if a type is a string (const char* is also considered a
  * string).
+ * @tparam T The type to check.
  */
 template <typename T>
-concept StringKind =
-  std::same_as<T, std::string> || std::same_as<T, const char *>;
+concept string_kind = same_as_any<T, std::string, const char *>;
 
 }  // namespace input_parser
 
